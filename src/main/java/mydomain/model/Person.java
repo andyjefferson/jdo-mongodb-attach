9afbc3153845bcb6a1f63a3ff18cba9e724de8e5
andyjefferson <andy@datanucleus.org>
2021-03-15 18:41:33 +0000

    initial files

---
+++ src/main/java/mydomain/model/Person.java
@@ -0,0 +1,33 @@
+package mydomain.model;
+
+import javax.jdo.annotations.*;
+
+@PersistenceCapable(detachable="true")
+public class Person
+{
+    @PrimaryKey
+    Long id;
+
+    String name;
+
+    public Person(long id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String n)
+    {
+        this.name = n;
+    }
+}
---
+++ src/test/java/org/datanucleus/test/SimpleTest.java
@@ -0,0 +1,107 @@
+package org.datanucleus.test;
+
+import java.util.*;
+import org.junit.*;
+import javax.jdo.*;
+
+import static org.junit.Assert.*;
+import mydomain.model.*;
+import org.datanucleus.util.NucleusLogger;
+
+public class SimpleTest
+{
+    @Test
+    public void testSimple()
+    {
+        NucleusLogger.GENERAL.info(">> test START");
+        PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("MyTest");
+
+        PersistenceManager pm = pmf.getPersistenceManager();
+        Transaction tx = pm.currentTransaction();
+        Person detachedP = null;
+        try
+        {
+            tx.begin();
+
+            Person p = new Person(1, "First");
+            pm.makePersistent(p);
+
+            pm.flush();
+
+            detachedP = pm.detachCopy(p);
+            tx.commit();
+        }
+        catch (Throwable thr)
+        {
+            NucleusLogger.GENERAL.error(">> Exception in test", thr);
+            fail("Failed test : " + thr.getMessage());
+        }
+        finally 
+        {
+            if (tx.isActive())
+            {
+                tx.rollback();
+            }
+            pm.close();
+        }
+        pmf.getDataStoreCache().evictAll();
+
+        detachedP.setName("Second");
+
+        pm = pmf.getPersistenceManager();
+        tx = pm.currentTransaction();
+        try
+        {
+            tx.begin();
+
+            NucleusLogger.GENERAL.info(">> Attaching Person");
+            Person p = pm.makePersistent(detachedP);
+            NucleusLogger.GENERAL.info(">> Attached Person : " + p);
+
+            tx.commit();
+        }
+        catch (Throwable thr)
+        {
+            NucleusLogger.GENERAL.error(">> Exception in test", thr);
+            fail("Failed test : " + thr.getMessage());
+        }
+        finally 
+        {
+            if (tx.isActive())
+            {
+                tx.rollback();
+            }
+            pm.close();
+        }
+        pmf.getDataStoreCache().evictAll();
+
+        pm = pmf.getPersistenceManager();
+        tx = pm.currentTransaction();
+        try
+        {
+            tx.begin();
+
+            NucleusLogger.GENERAL.info(">> Retrieving Person");
+            Person p = pm.getObjectById(Person.class, 1);
+            NucleusLogger.GENERAL.info(">> Retrieved Person : name=" + p.getName());
+
+            tx.commit();
+        }
+        catch (Throwable thr)
+        {
+            NucleusLogger.GENERAL.error(">> Exception in test", thr);
+            fail("Failed test : " + thr.getMessage());
+        }
+        finally 
+        {
+            if (tx.isActive())
+            {
+                tx.rollback();
+            }
+            pm.close();
+        }
+
+        pmf.close();
+        NucleusLogger.GENERAL.info(">> test END");
+    }
+}
